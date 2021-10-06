749c4d005dafdc2db19ca583e42b7c6128881075
Achal Shah <achals@gmail.com>
2021-10-05 21:08:23 -0700

    dirtiescontext
    Signed-off-by: Achal Shah <achals@gmail.com>

--- serving/src/test/java/feast/serving/it/ServingServiceFeast10IT.java
+++ serving/src/test/java/feast/serving/it/ServingServiceFeast10IT.java
@@ -37,6 +37,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -46,6 +47,7 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 
 @ActiveProfiles("it")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 @Testcontainers
 public class ServingServiceFeast10IT extends BaseAuthIT {
 
